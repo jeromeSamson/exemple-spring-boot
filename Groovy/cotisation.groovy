77373132785625fc89655367f07e7ddb8f663a5c
Jérôme SAMSON DIT LAVIGNE <jerome.samson07@gmail.com>
2017-08-16 14:53:12 +0200

    Spring boot

---
+++ Groovy/cotisation.groovy
@@ -0,0 +1,16 @@
+	@RestController
+	class CotisationsController {
+		@Autowired JdbcTemplate jdbcTemplate;
+		RowMapper<Cotisation> rowMapper = {rs,num -> return new Cotisation(
+		id:rs.getInt("ID"),
+		libelle:rs.getString("LIBELLE"),
+		code:rs.getString("CODE"),
+		tauxSalarial:rs.getString("TAUXSALARIAL"),
+		tauxPatronal:rs.getString("TAUXPATRONAL")
+		)}
+
+			@RequestMapping("/")
+			List<Cotisation> lister() {
+			return this.jdbcTemplate.query("select * from COTISATION", rowMapper)
+		}
+	}
